99078e4ffff2c77d87f0b12cb26ebd89f8f4d097
dreds-10 <andrecortellini@hotmail.com>
2014-02-03 17:33:47 -0500

    Final Digg Website
    My final Assignment

---
+++ js/my-script.js
@@ -0,0 +1,18 @@
+
+
+
+$(document).ready(function() { //when dom is ready	
+	
+	$("#close-banner").click(function(){
+		$(".site-promo-banner").css("top", "-100px");
+		$(".digg-header").css("top", "0px");
+		$("#content-main").css("padding-top", "53px");
+	});
+	
+	
+	
+	
+		
+
+});//WHEN DOM IS READY FUNCTION
+
